f1e503e5ca0b456ab50376260bb52dae3b640359
bohdan <bmonast@gmail.com>
2015-06-16 22:02:14 -0700

    add header; add libraries; use fiunction geoip instead of freegeoip

--- geoPlotGroups.R
+++ geoPlotGroups.R
@@ -1,37 +1,58 @@
 #!/usr/bin/Rscript
 
-# author : Bohdan Monastyrskyy
-# date   : 11/22/2014
-# description:
-#   the script generates statistics regarding the perfomance of the groups
-#   participating in CASP in RR category;
+
+# Description:
+#   the script generates statistics regarding the geolocation of the groups
+#   participating in CASP ;
 #   the data are fetched from predictioncenter.org database="predictioncenter"
 #      tables="casp11.groups,accounts"
-#   so make sure that you have uploaded the data through the web-interface
+#   
+################################################
+# Author : Bohdan Monastyrskyy
+# Date   : 11/22/2014
 
 
-# load libraries
-require(RPostgreSQL);
-require(plyr);
-require(ggplot2);
+# load CRAN packageslibraries
+packs <- c("RPostgreSQL", "dplyr", "ggplot2", "maps", "mapdata", "maptools")
+for (p  in packs){
+  if (!require(p, character.only = TRUE)){
+    install.packages(p);
+    if (!require(p, character.only = TRUE)){
+      stop(paste0("failed to load required package: ", p))
+    }
+  }
+}
 
-# load iuser-defined libraries
+# load user-defined libraries
 source("functions/GroupsUtils.R");
 source("functions/Utils.R");
+
+# load config.R
+# the variables realted to database: HOST_IP, DB_USER, DB_NAME
+source("config.R")
+
+# ---------------------------------------------------
+# RETRIEVE DATA
+# 
+# retrieve data from database at predictioncenter.org
+# and write data.frame to file 
+# ---------------------------------------------------
 
 # set up database driver
 drv <- dbDriver("PostgreSQL")
 
 # set db connector
-con <- dbConnect(drv, host='128.120.136.187', user='predictioncenter', dbname='predictioncenter')
+con <- dbConnect(drv, host=HOST_IP, user=DB_USER, dbname=DB_NAME)
+# fetch data from database
 
-gr_info <- getGroupsIPs(con, 'casp11')
+gr_info <- getGroupsIPs(con, CASP)
+# search the geolocation by ip
+geoip <- sapply(gr_info$ip, FUN=ipinfo)
 
-geoip <- sapply(gr_info$ip, freegeoip)
+gr_info <- unique(merge(gr_info, geoip, by=c("ip"))
 
-gr_info$longit <- as.numeric(as.character(geoip$longitude))
-gr_info$latit <- as.numeric(as.character(geoip$latitude))
+write.csv(gr_info, paste0("data/",CASP,".geo_data.txt", quote=FALSE, row.names=FALSE)
 
-write.csv(gr_info, "casp11.geo_data.txt", quote=FALSE, row.names=FALSE)
-
-
+# ---------------------------------------------------
+# END RETRIEVE DATA
+# ---------------------------------------------------
