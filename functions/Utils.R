73ea0bf8f2505c25588a3030437a48a38ef803d1
bohdan <bmonast@gmail.com>
2015-06-16 21:30:17 -0700

    add function ipinfo in functions/Utils.R

--- functions/Utils.R
+++ functions/Utils.R
@@ -2,31 +2,79 @@
 # service to resolve an IP address (or a vector of them) 
 # into country, region, city, zip, latitude, longitude, 
 # area and metro codes.
+# Author: Heuristic Andrew (thank you)
 freegeoip <- function(ip, format = ifelse(length(ip)==1,'list','dataframe'))
 {
-    if (1 == length(ip))
-    {
-        # a single IP address
-        if (require("rjson") != 1){
-		install.packages("rjson")
-		require("rjason")
-	}
-        url <- paste(c("http://freegeoip.net/json/", ip), collapse='')
-        ret <- fromJSON(readLines(url, warn=FALSE))
-        if (format == 'dataframe')
-            ret <- data.frame(ret)
-        return(ret)
-    } else {
-        ret <- data.frame()
-        for (i in 1:length(ip))
-        {
-            r <- freegeoip(ip[i], format="dataframe")
-            ret <- rbind(ret, r)
-        }
-        return(ret)
+  # load required libraries
+  if (require("rjson") != 1){
+    install.packages("rjson")
+    require("rjson")
+  }
+  # if a single IP address  
+  if (1 == length(ip)){        
+    url <- paste(c("http://freegeoip.net/json/", ip), collapse='')
+    result <- fromJSON(readLines(url, warn=FALSE))
+    if (format == 'dataframe'){
+          result <- data.frame(result)
     }
+    return(result)
+  } else {
+    ret <- data.frame()
+    for (i in 1:length(ip)){
+        r <- freegeoip(ip[i], format="dataframe")
+        result <- rbind(result, r)
+    }
+    return(result)
+  }
 } 
 
 try.ip <- function(ip){ 
   suppressWarnings(try(freegeoip(ip), silent = TRUE))
+}
+
+# another function to get latitude and longitude
+# requires RCurl package
+ipinfo <- function(ip, format="dataframe"){
+  if (!require("RCurl")){
+    tryCatch(
+      {
+        suppressWarnings(utils:install.packages("RCurl"))
+      },
+      warning = function(war){
+        print(paste0("WARNING: ", war));
+      },
+      error = function(err){
+        stop(paste0("load failed : ", "RCurl"));
+      },
+      finally = function(fin){
+        # do nothing
+      }
+    );
+    if (!require("RCurl")){
+      stop(paste0("loading failed : " , "RCurl"));
+    }
+  }
+  if (length(ip) == 1){
+    res <- getURL(paste("http://ipinfo.io/",ip,"/loc", sep=""));
+    #remove space symbols
+    res <- gsub("[[:space:]]", "", res);
+    if (res == "undefined"){
+      res <- c(NA, NA)
+    } else {
+      res <- strsplit(res, split=',')[[1]];
+    }
+    names(res) <- c("latit", "longit")
+    if (format == 'dataframe'){
+      result <- data.frame(t(res));
+      result$ip <- ip;
+      return(result);
+    }
+    return(res);
+  } else {
+    result <- data.frame()
+    for (ip1 in ip) {
+      result <- rbind(result, ipinfo(ip1, "dataframe")); 
+    }
+    return(result);
+  }
 }
