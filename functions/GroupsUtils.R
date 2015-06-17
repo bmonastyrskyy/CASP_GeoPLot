4bce6c24f5a3d4ea94ef3b0adea4844f316682c2
bohdan <bmonast@gmail.com>
2015-06-16 22:00:48 -0700

    minor changes realated to the format of query

--- functions/GroupsUtils.R
+++ functions/GroupsUtils.R
@@ -5,7 +5,8 @@
 # casp - casp's round 
 # prFormat - prediction format  
 getGroupsIPs <- function(dbCon, casp="casp11", prFrmat='(\'TS\', \'RR\',\'QA\')'){
-     query <- paste("select distinct ip, g.name, g.code, p.pfrmat from casp11.predictors pr join ", 
+     query <- paste0("select distinct ip, g.name, g.code, p.pfrmat from ",
+	casp, ".predictors pr join ", 
         casp, ".groups_predictors_relations gpr on gpr.predictors_id=pr.id join ",
 	casp, ".groups g on g.id=gpr.groups_id ", 
 	"join accounts a on pr.accounts_id=a.id ", 
