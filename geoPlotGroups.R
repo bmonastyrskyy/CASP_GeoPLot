a7f28d97ad6432f9a5ca8076bf5978b198ee8b03
bohdan <bmonast@gmail.com>
2015-06-25 13:58:59 -0700

    add mechanism to read data from db just onece; after read the data from file

--- geoPlotGroups.R
+++ geoPlotGroups.R
@@ -12,13 +12,15 @@
 # Date   : 11/22/2014
 
 
-# load CRAN packageslibraries
-packs <- c("RPostgreSQL", "dplyr", "ggplot2", "maps", "mapdata", "maptools")
-for (p  in packs){
+# load CRAN libraries
+libs <- c("RPostgreSQL", "dplyr", "ggplot2", "maps", 
+          "mapdata", "maptools", "rworldmap", 
+          "RCurl", "ggmap");
+for (p  in libs){
   if (!require(p, character.only = TRUE)){
     install.packages(p);
     if (!require(p, character.only = TRUE)){
-      stop(paste0("failed to load required package: ", p))
+      stop(paste0("failed to load required package: ", p));
     }
   }
 }
@@ -28,31 +30,50 @@ source("functions/GroupsUtils.R");
 source("functions/Utils.R");
 
 # load config.R
-# the variables related to database: HOST_IP, DB_USER, DB_NAME
+# the variables related to database: HOST_IP, DB_USER, DB_NAME, CASP
 source("config.R")
 
 # ---------------------------------------------------
-# RETRIEVE DATA
+# try to read data from file
+# ---------------------------------------------------
+dataFile <- paste0("data/",CASP,".geo_data.txt")
+skipDB <- FALSE # boolean flag to avoid retrieving data from DB
+if (file.exists(dataFile)){
+  skipDB <- TRUE
+}
+
+# ---------------------------------------------------
+# RETRIEVE DATA FROM DB
 # 
 # retrieve data from database at predictioncenter.org
 # and write data.frame to file 
 # ---------------------------------------------------
-
+if (! skipDB ){
 # set up database driver
 drv <- dbDriver("PostgreSQL")
 
 # set db connector
-con <- dbConnect(drv, host=HOST_IP, user=DB_USER, dbname=DB_NAME)
+con <- dbConnect(drv, host=HOST_IP, user=DB_USER, 
+                 dbname=DB_NAME)
 # fetch data from database
 
 gr_info <- getGroupsIPs(con, CASP)
 # search the geolocation by ip
-geoip <- sapply(gr_info$ip, FUN=ipinfo)
+geoip <- ipinfo(unique(gr_info$ip), format="dataframe")
 
-gr_info <- unique(merge(gr_info, geoip, by=c("ip"))
+gr_info <- merge(gr_info, geoip, by=c("ip"))
 
-write.csv(gr_info, paste0("data/",CASP,".geo_data.txt", quote=FALSE, row.names=FALSE)
-
+write.csv(gr_info, dataFile, 
+          quote=FALSE, row.names=FALSE)
+}
 # ---------------------------------------------------
-# END RETRIEVE DATA
+# END RETRIEVE DATA FROM DB
 # ---------------------------------------------------
+
+gr_info <- read.csv(dataFile, 
+                    header=TRUE, stringsAsFactors=FALSE)
+worldMap <- borders("world", colour="grey60", fill="grey70" )
+mp <- ggplot() + worldMap
+mp <- mp + geom_jitter(data=gr_info, aes(x=longit, y=latit), size = 3, color="red", position = position_jitter(width = 0.4, height = .4))
+print(mp)
+
